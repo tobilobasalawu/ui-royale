6cb6a8698992e096811ad233031f80b664c65509
John Name <oluwatobilobasalawu1234@gmail.com>
2025-02-16 23:01:19 +0000

    Intializing Firebase

---
+++ src/lib/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDVbyarsLs0oHVVb-uuhVDzUlXWIfMqXas",
+  authDomain: "ui-royale.firebaseapp.com",
+  projectId: "ui-royale",
+  storageBucket: "ui-royale.firebasestorage.app",
+  messagingSenderId: "910048060682",
+  appId: "1:910048060682:web:8436ec5a3d262fd29c86f3",
+  measurementId: "G-ZP4H3GVYEC"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);

c3579901322bfffe7d8d0bb54005f8590c35c2ee
Tobi <oluwatobilobasalawu1234@gmail.com>
2025-02-18 00:09:28 +0000

    Added User Authentication

---
+++ src/metadata.ts
@@ -0,0 +1,7 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "UI Royal",
+  description:
+    "Compete in real-time and customize stunning interfaces. Enter the arena and let the battle begin!",
+};

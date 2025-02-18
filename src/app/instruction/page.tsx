ee8299a2b2f7f3a21d76a639461c13d9248a55f6
Tobi <oluwatobilobasalawu1234@gmail.com>
2025-02-18 22:04:52 +0000

    ignore errord

--- src/app/instruction/page.tsx
+++ src/app/instruction/page.tsx
@@ -39,6 +39,7 @@ export default function InstructionPage() {
         className="font-marcellus px-6 py-3 bg-primary-gradient rounded-md uppercase text-[#111111] text-xl border border-[#9C8BF9] shadow-hero-button transition-transform hover:scale-105"
         rippleColor="#111111"
         duration=".5s"
+        onClick={() => router.push("/editor")}
       >
         Start Designing
       </RippleButton>
